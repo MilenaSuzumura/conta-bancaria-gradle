708775254067db3e90c2773e33316a8e802a92e3
milenasuzumura <milena.suzumura11@gmail.com>
2024-04-18 18:28:51 -0300

    Tentei configurar a dependencia

--- src/main/java/com/betrybe/bankaccount/AccountNumberFormatter.java
+++ src/main/java/com/betrybe/bankaccount/AccountNumberFormatter.java
@@ -1,5 +1,6 @@
 package com.betrybe.bankaccount;
 
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * AccountNumberFormatter.
@@ -11,8 +12,15 @@ public class AccountNumberFormatter {
    *
    * @param literalAccountNumber número da conta
    */
-  public String formatAccountNumber(int literalAccountNumber) {
-
+  public static String formatAccountNumber(int literalAccountNumber) {
+    String numberAccount = Integer.toString(literalAccountNumber);
+    if (numberAccount.length() < 6) {
+      return StringUtils.leftPad(numberAccount, 6, "0");
+    } else if (numberAccount.length() > 6) {
+      return StringUtils.right(numberAccount, 6);
+    } else {
+      return numberAccount;
+    }
   }
 
 }
